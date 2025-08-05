793be03c838b61e1a157c238bce390c7fcefe694
Hmzcck <hamzacicekqwe@gmail.com>
2025-08-05 13:01:15 +0300

    ECommerceEvent Model

---
+++ src/ECommerce.Models/ECommerceEvent.cs
@@ -0,0 +1,25 @@
+namespace ECommerce.Models;
+
+public record ECommerceEvent
+{
+    public required Guid UserId { get; init; }
+    public required string SessionId { get; init; }
+    public required EventTypes EventType { get; init; }
+    public string? ProductId { get; init; }
+    public string? Category { get; init; }
+    public decimal? Price { get; init; }
+    public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+    public required string IpAddress { get; init; }
+    public Dictionary<string, string>? Metadata { get; init; }
+
+    public ECommerceEvent() { }
+}
+
+public enum EventTypes
+{
+    PageView,
+    ProductSearch,
+    AddToCart,
+    Purchase,
+    RemoveFromCart,
+}
